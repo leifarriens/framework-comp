c4252a0e36ee3351c3e835a820ba1afb56f23fba
Leif Arriens <leif.arriens@gmail.com>
2023-02-07 03:17:11 +0100

    add: log

---
+++ scripts/write-log.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+
+function main() {
+  const log = filterLog();
+
+  writeOutput(log);
+}
+
+function filterLog() {
+  try {
+    const logfile = fs.readFileSync('./build.log', 'utf-8');
+
+    const rows = logfile.split('\n');
+
+    const jsoutput = rows.filter((row) => row.includes('.js'));
+
+    return jsoutput.join('\n');
+  } catch (error) {
+    throw new Error('No log file found');
+  }
+}
+
+function writeOutput(data: string) {
+  fs.writeFileSync('README.md', `# framework-comp\n\n${data}\n`);
+}
+
+main();
